822c11f6892c72fef7a52082f1deab9417c1f683
1986allex <1986allex@gmail.com>
2017-09-20 21:20:20 +0300

    init

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 
