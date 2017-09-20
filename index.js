401ea2804bff7b224d7da738da90ebf4368a1112
1986allex <1986allex@gmail.com>
2017-09-20 21:58:04 +0300

    init

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 var express = require("express");
 var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
-
-/*var express = require("express");
-var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var turnState = true;
@@ -122,4 +108,4 @@ res.sendFile(__dirname + '/index.html');
 	
 http.listen(3000, function(){
   console.log("listening on *:3000");
-});*/
+});
